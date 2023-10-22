755a420c79893e156dd142231f050b03ffbb7a3c
vahinala <vahinalaralison11@gmail.com>
2023-10-22 20:46:14 +0300

    update 3

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-import translate from '@mgcodeur/super-translator';
-import express from 'express';
-const app = express();
-app.use(express.json());
+const translate = require('@mgcodeur/super-translator');
+const express = require('express');
 
+const app = express();
 var donneesStockees = [];
+
+//////////////////////////////////
+// permettre l'accès à l'API (CORS)
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Max-Age", "1800");
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token,Origin, X-Requested-With, Content, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  
+  app.use((req, res) => {
+    res.json({ message: "UPDATE !" }); 
+ });
 
 // Route pour recevoir des données en POST
 app.post('/envoyer-donnees', (req, res) => {
-    console.log("*******************");
     const donneesRecues = req.body;
     donneesStockees = donneesRecues;
     res.status(201).json({ message: 'Données reçues avec succès' });
@@ -23,10 +37,6 @@ app.get('/recuperer-donnees',async (req, res) => {
     return el;
   }));
   res.status(200).json(results);
-});
-
-app.use((req, res) => {
-  res.json({ message: "UPDATE !" }); 
 });
 
 module.exports = app;
